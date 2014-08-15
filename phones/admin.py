132fb170abc744feebdcebfacea6ada35f09b06b
chingalo <profschingalo@gmail.com>
2014-08-15 13:34:44 +0300

    add more configuratins for urls and modelling

---
+++ phones/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from phones.models import *
+
+#admin.site.register(Users)
--- phones/views.py
+++ phones/views.py
@@ -1,1 +1,16 @@
-# Create your views here.
+from django.http import HttpResponseRedirect,HttpResponse
+from django.shortcuts import render_to_response, get_object_or_404, render
+from django.core.context_processors import csrf
+from django.core.urlresolvers import reverse
+import json 
+from django.core.mail import send_mail
+from random import randrange
+from datetime import datetime
+from reportlab.pdfgen import canvas
+from reportlab.lib import colors
+from reportlab.lib.pagesizes import letter
+from reportlab.lib.enums import TA_CENTER
+from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, Image, Table, TableStyle
+from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
+from phones.models import *
+
