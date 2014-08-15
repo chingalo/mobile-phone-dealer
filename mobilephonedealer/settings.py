c051e3b07b2283d5fef5988a629af8d84ccd24ab
chingalo <profschingalo@gmail.com>
2014-08-15 13:51:40 +0300

    update for mob

--- mobilephonedealer/settings.py
+++ mobilephonedealer/settings.py
@@ -13,10 +13,13 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql', 
-        'NAME': 'mobiletz',  
-        'USER': 'mobiletz',
-        'PASSWORD': 'mobiletz',
+        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': '',                      # Or path to database file if using sqlite3.
+        # The following settings are not used with sqlite3:
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': '',                      # Set to empty string for default.
     }
 }
 ALLOWED_HOSTS = []
@@ -111,3 +114,15 @@ LOGGING = {
         },
     }
 }
+
+
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
