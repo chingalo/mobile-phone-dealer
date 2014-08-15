a46a7f967436a1643f61578b20c7773e2c83844d
chingalo <profschingalo@gmail.com>
2014-08-15 12:56:57 +0300

    initial ste up for mobiletz

--- mobilephonedealer/urls.py
+++ mobilephonedealer/urls.py
@@ -1,17 +1,25 @@
 from django.conf.urls import patterns, include, url
+from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.contrib import admin
+admin.autodiscover()
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'mobilephonedealer.views.home', name='home'),
-    # url(r'^mobilephonedealer/', include('mobilephonedealer.foo.urls')),
+    
+   # url(r'', include('reports.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+    
+    
+)+ staticfiles_urlpatterns()
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
-)
+if settings.DEBUG:
+    urlpatterns = patterns('',
+		
+        url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
+        {'document_root': settings.MEDIA_ROOT,
+        'show_indexes': True}),
+        url(r'',
+        include('django.contrib.staticfiles.urls')),
+    ) + urlpatterns
--- mobilephonedealer/wsgi.py
+++ mobilephonedealer/wsgi.py
@@ -20,13 +20,11 @@ import os
 # mod_wsgi daemon mode with each site in its own daemon process, or use
 # os.environ["DJANGO_SETTINGS_MODULE"] = "mobilephonedealer.settings"
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mobilephonedealer.settings")
-
-# This application object is used by any WSGI server configured to use this
-# file. This includes Django's development server, if the WSGI_APPLICATION
-# setting points here.
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
+from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
 
-# Apply WSGI middleware here.
-# from helloworld.wsgi import HelloWorldApplication
-# application = HelloWorldApplication(application)
+application = Cling(get_wsgi_application())
+
+
