a9664dd070e24bc72aabba0349b34085ed346f81
chingalo <profschingalo@gmail.com>
2014-08-19 09:43:00 +0300

    remodelling mobile tz to accomodate dealers

--- phones/models.py
+++ phones/models.py
@@ -1,9 +1,24 @@
 from django.db import models
 
+class Dealer(models.Model):
+	name = models.CharField(max_length = 200)
+	e_mail = models.EmailField(max_length = 200)
+	mobile_number = models.CharField(max_length = 200)
+	username = models.CharField(max_length = 200)
+	password = models.CharField(max_length = 200)
+	login_status = models.CharField(max_length = 200, default = 'log_out')
+	account_ status = models.CharField(max_length = 200, default = 'active')
+	
+	def __unicode__(self):
+		return self.name
+	
+	
+
 class Phone(models.Model):
+	dealer = models.ForeignKey('Dealer',on_delete = models.CASCADE)
 	brand_of_Phone = models.CharField(max_length = 200)
 	model_of_Phone = models.CharField(max_length = 200)
-	description = models.TextField(max_length = 20000)
+	description = models.TextField(max_length = 20000, blank = True)
 	quantity = models.IntegerField(max_length = 200)
 	price = models.IntegerField(max_length = 200)
 	status = models.CharField(max_length = 200 , default = 'new')
