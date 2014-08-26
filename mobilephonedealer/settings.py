79b88db949dc5f2135748b691a6440a58bb24c8b
chingalo <profschingalo@gmail.com>
2014-08-26 09:55:14 +0300

    modify database setting for local server

--- mobilephonedealer/settings.py
+++ mobilephonedealer/settings.py
@@ -13,15 +13,13 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        # The following settings are not used with sqlite3:
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'mobiletz',  
+        'USER': 'mobiletz',
+        'PASSWORD': 'mobiletz',
     }
 }
+
 ALLOWED_HOSTS = []
 
 TIME_ZONE = 'Africa/Dar_es_Salaam'
@@ -119,12 +117,6 @@ LOGGING = {
 }
 
 
-# Parse database configuration from $DATABASE_URL
-import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
-
-# Honor the 'X-Forwarded-Proto' header for request.is_secure()
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Allow all host headers
 ALLOWED_HOSTS = ['*']
